1940e9d9870d3342bb255be331e4b0a29352b141
chanderbhanu096 <chanderbhanu096@gmail.com>
2020-07-10 16:51:22 +0530

    Update nameChecker.js
    Added Test Checker

--- src/client/js/nameChecker.js
+++ src/client/js/nameChecker.js
@@ -8,5 +8,17 @@ function checkForName(inputText) {
        return false;
    }
 }
+// to check whether the user input is empty or not
+function checkForUserInput(inputText, setErrorMessage = true) {
+    console.log('Check for user text', inputText);
+    //user input is not empty
+    if(!inputText) {
+        if(setErrorMessage) {
+            handleInputError('Please Enter Something');
+        }
+        return false;
+    }
+    return true;
+}
 
-export { checkForName }
+export { checkForName,checkForUserInput }
